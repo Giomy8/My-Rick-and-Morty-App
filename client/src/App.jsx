0d28ac18758c143c00ad53e5a3214b46ef9e0d62
Giomy8 <giomarmesa@gmail.com>
2023-07-18 00:21:13 -0500

    Async Await

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,66 +12,67 @@ import HeaderIndex from './components/HeaderIndex/HeaderIndex'
 import { useDispatch } from 'react-redux';
 import { removeFav } from './redux/actions/actions';
 
-
-
 export default function App() {
+   const [characters , setCharacters] = useState ([]);
 
-   function login(userData) {
-      const { email, password } = userData;
-      const URL = 'http://localhost:3001/rickandmorty/login/';
-      axios(URL + `?email=${email}&password=${password}`).then(({ data }) => {
+   const dispatch = useDispatch();
+   const navigate = useNavigate();
+   const { pathname } = useLocation();
+
+   const [access, setAccess] = useState(false);
+   useEffect(() => {
+      !access && navigate('/');
+   }, [access]);
+
+   async function login(userData) {
+      try {
+         const { email, password } = userData;
+         const URL = 'http://localhost:3001/rickandmorty/login/';
+         const { data } = await axios.get(URL + `?email=${email}&password=${password}`);
          const { access } = data;
          setAccess(data);
          access && navigate('/home');
-      });
+      } catch (err) {
+         console.error(err);
+         return res.status(500).send(`Error: ${err.message}`);
+      }
    }
 
-
-const [characters , setCharacters] = useState ([]);
-
-const dispatch = useDispatch();
-const navigate = useNavigate();
-const {pathname} = useLocation();
-
-
-const [access, setAccess] = useState(false);
-useEffect(() => {
-   !access && navigate('/');
-}, [access]);
-
-function onSearch(character) {
-   axios(`http://localhost:3001/rickandmorty/character/${character}`)
-   .then((response) =>{ 
-   const data = response.data;
-   console.log(data);
+   async function onSearch(character) {
+      try {
+         const response = await axios.get(`http://localhost:3001/rickandmorty/character/${character}`);
+         const data = response.data;
+         console.log(data);
    
-      if (data.id) {
-         setCharacters((oldChars) =>[...oldChars, data]);
-      } else {
-         alert('There are no characters with this ID!');
+         if (data.id) {
+            setCharacters((oldChars) => [...oldChars, data]);
+         } else {
+            alert('There are no characters with this ID!');
+         }
+      } catch (err) {
+         console.error(err);
       }
-   })
-   .catch(err => console.error(err))
-}
+   }
 
-const onClose = (id) => {
-      let cardsfiltradas = characters.filter((character) =>    character.id !== Number(id))
-      setCharacters([...cardsfiltradas])
-      dispatch(removeFav(id))
-}
+   const onClose = (id) => {
+      let cardsfiltradas = characters.filter((character) => character.id !== Number(id));
+      setCharacters([...cardsfiltradas]);
+      dispatch(removeFav(id));
+   };
+
    return (
-      <div className = "App">
-        {pathname !== '/' && <Nav onSearch={onSearch} setAccess={setAccess}/>}
-        {pathname === '/' && <HeaderIndex />} 
+      <div className="App">
+         {pathname !== '/' && <Nav onSearch={onSearch} setAccess={setAccess} />}
+         {pathname === '/' && <HeaderIndex />} 
          <Routes>
-           <Route path="/" element={<Form setAccess={setAccess} login={login}/>} />
-           <Route path="/home" element={<Cards characters={characters} onClose={onClose} />} />
-           <Route path="/about" element={<About />} />
-           <Route path="/detail/:id" element={<Detail />} />
-           <Route path="/favorites" element={<Favorites onClose={onClose}/>}/>
-           </Routes>
-         
+            <Route path="/" element={<Form setAccess={setAccess} login={login} />} />
+            <Route path="/home" element={<Cards characters={characters} onClose={onClose} />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/detail/:id" element={<Detail />} />
+            <Route path="/favorites" element={<Favorites onClose={onClose} />} />
+         </Routes>
       </div>
    );
 }
+
 
--- server/src/controllers/getCharById.js
+++ server/src/controllers/getCharById.js
@@ -2,32 +2,33 @@ const axios = require('axios');
 
 const URL = "https://rickandmortyapi.com/api/character/";
 
-const getCharById = (req, res) => {
-  const { id } = req.params;
+const getCharById = async (req, res) => {
+  try {
+    const { id } = req.params;
 
-  axios.get(`${URL}${id}`)
-    .then(response => {
-      const data = response.data;
-      if (Object.keys(data).length === 0) {
-        return res.status(404).json({ message: "Not found" });
-      }
+    const response = await axios.get(`${URL}${id}`);
+    const data = response.data;
 
-      const character = {
-        id: data.id,
-        status: data.status,
-        name: data.name,
-        species: data.species,
-        origin: data.origin,
-        image: data.image,
-        gender: data.gender
-      };
+    if (Object.keys(data).length === 0) {
+      return res.status(404).json({ message: "Not found" });
+    }
 
-      return res.status(200).json(character);
-    })
-    .catch(err => {
-      return res.status(500).send(`Error: ${err.message}`);
-    });
+    const character = {
+      id: data.id,
+      status: data.status,
+      name: data.name,
+      species: data.species,
+      origin: data.origin,
+      image: data.image,
+      gender: data.gender
+    };
+
+    return res.status(200).json(character);
+  } catch (err) {
+    return res.status(500).send(`Error: ${err.message}`);
+  }
 };
 
 module.exports = getCharById;
+
 
