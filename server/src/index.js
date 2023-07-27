5cb0788260074e5aafbd80cdd9135dbe63b8416d
Giomy8 <giomarmesa@gmail.com>
2023-07-27 02:28:01 -0500

    Requelize

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,17 @@
 const server = require('./app');
+const { conn } = require('./DB_connection');
 const PORT = 3001;
 
 
-server.listen(PORT, () => {
-    console.log(`Server raised in port: ${PORT}`);
-});
+conn.sync({ force: false }).then(() => {
+    console.log('conexion a base de datos')
+    server.listen(PORT, () => {
+        console.log(`Server raised in port: ${PORT}`);    
+    });
+}
+);
+
+
 
 
 // const express = require("express");
